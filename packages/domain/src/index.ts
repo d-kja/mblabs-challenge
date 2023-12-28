88cfea7742376d6add0cbbdcf7556f77c285094b
d-kja <89911360+d-kja@users.noreply.github.com>
2023-12-28 15:39:39 -0300

    refactor(packages/domain): export domain classes through index file

---
+++ packages/domain/src/index.ts
@@ -0,0 +1,13 @@
+//
+// EXPORTING DOMAIN CLASSES AND ENTITIES SO THAT IT CAN BE USED AS A PACKAGE
+//
+
+export * from './domain/classes/application/repositories/result-repository.js'
+
+export * from './domain/classes/application/use-cases/create-result.service.js'
+export * from './domain/classes/application/use-cases/delete-result.service.js'
+export * from './domain/classes/application/use-cases/list-results.service.js'
+
+export * from './domain/classes/enterprise/entities/result.js'
+export * from './domain/classes/enterprise/entities/value-object/bimester.js'
+export * from './domain/classes/enterprise/entities/value-object/class-type.js'
