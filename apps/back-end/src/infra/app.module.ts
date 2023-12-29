e2843d9cbd1a96a0b8adda0886a437744ba21e60
d-kja <89911360+d-kja@users.noreply.github.com>
2023-12-28 23:54:40 -0300

    chore(apps/backend): create initial files

---
+++ apps/back-end/src/infra/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
+
+@Module({
+  imports: [
+    EnvModule,
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
